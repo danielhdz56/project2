f07cd4e15b0a9e8558429a5c095f2239c123b9a9
captainsalt <dontbothertheking@gmail.com>
2017-10-06 15:01:03 -0500

    Made the vue page the default route

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  app.get("/vue", function(req, res) {
+  app.get("/", function(req, res) {
     const data = {
       text: "The vue page works!"
     };
