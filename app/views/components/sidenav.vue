b8fed6dcc5597f78353230c5df8869e841e51eb3
captainsalt <dontbothertheking@gmail.com>
2017-10-07 12:06:37 -0500

    Dh/19 add sidenav component (#25)
    * -add student, teacher, stupost, teapost tables (#21)
    
    * Add sidenav component and components folder

---
+++ app/views/components/sidenav.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div id="sidenav"> 
+    <h3 class="sidenav-heading">Menu</h3>
+    <a href="#" class="sidenav-link">Grades</a>
+    <a href="#" class="sidenav-link">Attendance</a>
+    <a href="#" class="sidenav-link">Homework</a>
+    <a href="#" class="sidenav-link">Messages</a>
+    <a href="#" class="sidenav-link">Roster</a>    
+    <a href="#" class="sidenav-link">Settings</a>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+        }
+    }
+}
+</script>
