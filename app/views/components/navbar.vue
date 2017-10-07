76b4dfb91cb4b97797ce3a54e24754229beb668d
captainsalt <dontbothertheking@gmail.com>
2017-10-07 12:08:24 -0500

    Login and navbar

---
+++ app/views/components/navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <li :key="option" v-for="option in options">{{option}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            options: [
+                "Home",
+                "Login",
+                "Sign up"
+            ]
+        }
+    }
+}
+</script>
+
---
+++ app/views/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Hello
+        <nav-component></nav-component>
+    </div>
+</template>
+
+<script>
+// const path = require("path");
+// import nav from path.join(__dirname, "components/navbar");
+
+import nav from "./components/navbar";
+
+export default {
+    data() {
+        return {
+        }
+    },
+    components: {
+        navComponent: nav
+    }
+}
+</script>
+
+<style>
+
+</style>
