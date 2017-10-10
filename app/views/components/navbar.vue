aaf49c04c0a1b50559f7293fc24ada5915ef42f2
captainsalt <dontbothertheking@gmail.com>
2017-10-10 00:50:17 -0500

    Created inline navbar with light background

--- app/views/components/navbar.vue
+++ app/views/components/navbar.vue
@@ -1,9 +1,11 @@
 <template>
-    <ul>
-        <li v-for="option in options">
-            <a :href="option.href" class="nav-link">{{option.name}}</a>
-        </li>
-    </ul>
+    <div id="grid">
+        <ul class="main-nav">
+            <li v-for="option in options">
+                <a :href="option.href" class="nav-link">{{option.name}}</a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -28,8 +30,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+#grid {
+    display: grid;
+    grid-template-columns: 70vw 30vw;
+    grid-template-areas: ". nav";
+    background: #eee;
+    padding: 5px 0;
+}
 
+.main-nav {
+    grid-area: nav;
+    display: flex;
+    justify-content: space-around;
+}
+
+.main-nav li a {
+    text-decoration: none;
+    color: black;
+}
+
+.main-nav li a:hover {
+    text-decoration: underline;
+    color: red;
+}
 </style>
-
-
