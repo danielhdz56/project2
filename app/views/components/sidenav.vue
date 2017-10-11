c33d5aa0da7424b85567df6e7cea568c7efbb7a7
captainsalt <dontbothertheking@gmail.com>
2017-10-11 18:09:11 -0500

    Dw/24 css grid (#43)
    * Added grid to home page
    
    * changed navbar to nav-bar
    
    * removed sidenav link class

--- app/views/components/sidenav.vue
+++ app/views/components/sidenav.vue
@@ -1,9 +1,9 @@
 <template lang="html">
-  <div id="sidenav"> 
-    <h3 class="sidenav-heading">Menu</h3>
+  <div id="side-nav"> 
+    <h3 id="header">Menu</h3>
     <ul>
       <li v-for="link in links">
-        <a :href="link.href" :class="link.classes">{{link.name}}</a>
+        <a :href="link.href">{{link.name}}</a>
       </li>
     </ul>
   </div>
@@ -11,38 +11,67 @@
 
 <script>
 export default {
-  data: function () {
+  data() {
     return {
       links: [{
         href: '#',
-        classes: 'sidenav-link',
         name: 'Grades'
       }, {
         href: '#',
-        classes: 'sidenav-link',
         name: 'Attendance'
 
       }, {
         href: '#',
-        classes: 'sidenav-link',
         name: 'Homework'
 
       }, {
         href: '#',
-        classes: 'sidenav-link',
         name: 'Messages'
 
       }, {
         href: '#',
-        classes: 'sidenav-link',
         name: 'Roster'
 
       }, {
         href: '#',
-        classes: 'sidenav-link',
         name: 'Settings'
       }]
     }
   }
 }
 </script>
+
+<style scoped>
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#side-nav {
+  background: #ccc;
+  display: grid;
+  grid-template-rows: 5% 95%;
+}
+
+#side-nav ul {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+#side-nav ul li {
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#side-nav ul li:nth-child(even) {
+  background: mediumslateblue;
+}
+
+#side-nav ul li:nth-child(odd) {
+  background: cornflowerblue;
+}
+</style>
