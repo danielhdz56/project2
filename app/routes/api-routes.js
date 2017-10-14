83bc6dc6b8c18039d4168915aae3eeef89bb6af8
Daniel Hernandez <dhernandez@WHOU-9408-WKS.local>
2017-10-14 12:26:27 -0500

    add api route for all users

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -4,30 +4,17 @@
 
 // Dependencies
 // =============================================================
+var db = require('../models/index');
 const User = require('../models/user.js');
-const Group = require('../models/group.js');
-const Class = require('../models/class.js');
-const Department = require('../models/department.js');
-const Post = require('../models/post.js');
-const Attendance = require('../models/attendance.js');
+
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 //Get all students' info
-app.get("/api/user",(request,response)=>{
-User.findAll({}).then((results)=>{
-    response.json(results);
-});
-});
-//Get all teachers' info
-app.get("/api/teachers",(request,response)=>{
-    Teacher.findAll({}).then((results)=>{
-        response.json(results);
+    app.get("/api/user",(request,response)=>{
+        User.findAll({}).then((results)=>{
+            response.json(results);
+        });
     });
-    });
-//Get info on one specific user
-app.get("/api/:user",(request,response)=>{
-    
-});
-};//end of module.exports = function(app)
+}//end of module.exports = function(app)
