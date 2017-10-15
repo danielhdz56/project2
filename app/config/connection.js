f377c1267daa682ca5e8d9858b57222e1459856e
Tish McNeel <latisha.mcneel@gmail.com>
2017-10-15 13:25:24 -0500

    Rearranging sequelize files and setting up relations

--- app/config/connection.js
+++ app/config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("chalkboardplus_db", "root", "password", {
+var sequelize = new Sequelize("chalkboardplus_db", "root", "rootpassword", {
   host: "localhost",
   dialect: "mysql",
   pool: {
--- app/models/post.js
+++ app/models/post.js
@@ -1,29 +1,18 @@
-// Dependencies
-// =============================================================
+module.exports = function(sequelize, DataTypes){
+  // Creates a "Post" model that matches up with DB
+  var Post = sequelize.define("post", {
+    content: {
+      type: DataTypes.STRING
+    },
+    attachment: {
+      type: DataTypes.STRING
+    }
+  });
 
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
+  //Post can belong to only one user
+  Post.associate = function(models){
+    Post.belongsTo(models.user);
+  };
 
-// Creates a "Post" model that matches up with DB
-var Post = sequelize.define("post", {
-  user_id: {
-    type: Sequelize.STRING
-  },
-  content: {
-    type: Sequelize.STRING
-  },
-  attachment: {
-    type: Sequelize.STRING
-  }, 
-  createdAt: Sequelize.DATE,
-  updatedAt: Sequelize.DATE
-});
-
-//Post can belong to only one user
-Post.associate = function(models){
-Post.belongsTo(models.User);
-}
-
-// Makes the Model available for other files (will also create a table)
-module.exports = Post;
+  return Post;
+};
