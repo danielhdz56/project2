82263f9a2f08eb3aa8acf8320288b849c7635305
captainsalt <dontbothertheking@gmail.com>
2017-10-15 16:11:21 -0500

    Attendace changes

--- app/views/components/attendance.vue
+++ app/views/components/attendance.vue
@@ -1,13 +1,26 @@
 <template>
-    <table id="table-attendance">
+    <table>
         <thead>
-            <tr v-for="row in rowsHead">
-                <th :colspan="header.colspan" :rowspan="header.rowspan" v-for="header in row.headers">{{header.name}}</th>
+            <tr>
+                <th rowspan="3">Student ID</th>
+                <th rowspan="3">Student Name</th>
+                <th rowspan="1" colspan="3">Attendance</th>
+            </tr>
+
+            <tr>
+                <th>Present</th>
+                <th>Absent</th>
+                <th>Tardy</th>
             </tr>
         </thead>
+
         <tbody>
-            <tr v-for="row in rowsBody">
-                <td v-for="dataCell in row.dataCells">{{dataCell.name}}</td>
+            <tr :key="student.ID" v-for="student in students">
+                <td>{{student.ID}}</td>
+                <td>{{student.name}}</td>
+                <td>{{student.present}}</td>
+                <td>{{student.absent}}</td>
+                <td>{{student.tardy}}</td>
             </tr>
         </tbody>
     </table>
@@ -15,47 +28,22 @@
 
 <script>
 
-
 export default {
     data() {
         return {
-            rowsHead: [{
-                headers: [{
-                        name: 'Student ID',
-                        rowspan: '3'
-                    }, {
-                        name: 'Student Name',
-                        rowspan: '3'
-                    },
-                    {
-                        name: 'Attendance',
-                        rowspan: '1',
-                        colspan: '3'
-                    }
-                ]}, {
-                headers: [{
-                    name: 'Present',
-                    rowspan: ''
-                }, {
-                    name: 'Absent',
-                    rowspan: ''
-                }, {
-                    name: 'Tardy',
-                    rowspan: ''
-                }]
-            }],
-            rowsBody: [{
-                dataCells: [{
-                    name: '12345' // id
-                }, {
-                    name: 'foo' // name
-                }, {
-                    name: '1' // Present
-                }, {
-                    name: '0' // Absent
-                }, {
-                    name: '0' // Tardy
-                }]
+            students: [{
+                ID: "W0908254",
+                name: "David Wright",
+                present: 50000,
+                absent: 0,
+                tardy: -30
+            },
+            {
+                ID: "H3212543",
+                name: "Daniel Hernandez",
+                present: 0,
+                absent: 7000,
+                tardy: 80000000
             }]
         }
     }
@@ -63,40 +51,6 @@ export default {
 </script>
 
 <style scoped>
-    #table-attendance {
-        border: solid thin;
-        border-collapse: collapse;
-        width: 100%;
-    }
 
-    #table-attendance th,
-    #table-attendance td {
-        border: solid thin;
-        padding: 0.5rem 2rem;
-        background: whitesmoke;
-    }
-
-    #table-attendance td {
-        white-space: nowrap;
-    }
-
-    #table-attendance th {
-        font-weight: normal;
-    }
-
-    #table-attendance td {
-        border-style: none solid;
-        vertical-align: top;
-    }
-
-    #table-attendance th {
-        padding: 0.2em;
-        vertical-align: middle;
-        text-align: center;
-    }
-
-    #table-attendance tbody td:first-child::after {
-        content: leader(". ");
-    }
 </style>
 
