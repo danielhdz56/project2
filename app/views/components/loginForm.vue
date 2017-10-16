e49175b804aff572a77f9d94f998ee770d2c6cdd
captainsalt <dontbothertheking@gmail.com>
2017-10-15 23:20:58 -0500

    Finished login page

--- app/views/components/loginForm.vue
+++ app/views/components/loginForm.vue
@@ -2,18 +2,19 @@
   <div id="login-container">
 
     <div id="login-form">
-      
-      <div id="usernamecontainer">
-        <label for="username"></label><br>
+      <h2 id="header">Sign in</h2>
+
+      <div id="username-container">
+        <label for="username">Username</label><br>
         <input type="text" id="username">
       </div>
 
       <div id="password-container">
-        <label for="password"></label>
+        <label for="password">Password</label><br>
         <input type="password" id="password">
       </div>
 
-      <button id="sumbit">Submit</button>
+      <input type="button" value="Go">
     </div>
   </div>
 </template>
@@ -28,6 +29,37 @@ export default {
 </script>
 
 <style>
+body {
+  background: url("https://hdwallsource.com/img/2014/8/colorful-ios-wallpaper-22410-23023-hd-wallpapers.jpg");
+}
+
+label,
+button,
+#header {
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+}
+
+label {
+  text-align: center;
+  width: 100%;
+}
+
+input {
+  border: none;
+  margin: 5px 0;
+  border-radius: 2px;
+  padding: 5px 2px;
+  width: 100%;
+}
+
+input[type=button] {
+  grid-area: btn;
+  background: #bfb;
+  color: darkslategray;
+  font-size: x-large;
+}
+
 #login-container {
   display: flex;
   align-items: center;
@@ -38,9 +70,36 @@ export default {
 }
 
 #login-form {
-  background: #24292e;
+  background: rgba(41,45,50, 0.6);
   border-radius: 3px;
-  width: 70%;
-  display: grid
+  padding: 30px;
+  width: 50%;
+  min-width: 600px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr) 1fr;
+  grid-template-areas: "head head user user" "head head pass pass" "head head btn btn";
+}
+
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  grid-area: head;
+  font-size: x-large;
+  border-right: 2px solid white;
+  margin-right: 30px;
+  font-weight: 600;
+  height: 240px;
+}
+
+#username-container {
+  grid-area: user;
+  width: 100%
+}
+
+#password-container {
+  grid-area: pass;
+  width: 100%;
 }
 </style>
